807a25d694103f989f5b6a90c6046f745e04ebd3
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-02 14:20:00 +0200

    Create makeGrid function

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const rangeText = document.querySelector(".range-text");
 const container = document.querySelector(".container");
 const showGridBtn = document.querySelector("#show-grid");
 const clearBtn = document.querySelector("#clear-grid");
+
+function makeGrid(size) {
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        const sizeCell = 600 / size;
+        cell.style.width = sizeCell + "px";
+        cell.style.height = sizeCell + "px";
+        cell.id = i + 1;
+        if (showGridBtn.textContent === "Hide Grid") {
+            cell.classList.add("cell");
+        }
+        container.appendChild(cell)
+    }
+}
