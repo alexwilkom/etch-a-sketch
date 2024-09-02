17e2385a5e93180f3378e25be9a3d60fccbd763a
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-02 14:19:03 +0200

    Declare variables to get the elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const range = document.querySelector("#range-value");
+const rangeText = document.querySelector(".range-text");
+const container = document.querySelector(".container");
+const showGridBtn = document.querySelector("#show-grid");
+const clearBtn = document.querySelector("#clear-grid");
